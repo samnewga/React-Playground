c8579fa3223a3d5a62f487b87be93b4d1dfcba2c
samnewga <newgate90s@gmail.com>
2020-02-04 12:56:09 -0700

    accordion added

--- src/App.js
+++ src/App.js
@@ -8,22 +8,56 @@ import Counter from './state/Counter';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
+import Tabs from './state/Tabs';
+import Accordion from './state-drills/Accordion';
 
 const firstTooltip = (
   <Tooltip color='hotpink' message='tooltip message'>
    ipsum
   </Tooltip>
-)
+);
 const secondTooltip = (
   <Tooltip color='#126BCC' message='another tooltip message'>
     officiis
   </Tooltip>
-)
+);
+
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+];
 
 function App() {
   return (
 
     <main className='App'>
+      <div className='wrap'>
+      <Accordion sections={sections}/>
+       </div> 
+
+      <div className='wrap'>
+      <Tabs tabs={tabsProp}/>
+      </div>
+
       <div className='wrap'>
       <HelloWorld />
        </div> 
